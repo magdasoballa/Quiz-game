42f8885cff1400d1827251e5f10ab617c626f9ae
magda <soballamagda@gmail.com>
2020-10-08 19:07:17 +0200

    finished game

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,14 +1,34 @@
-import React, {useState} from 'react'
+import React, {useState, useRef, useEffect} from 'react'
 
 export function Card  ({card})  {
     const [flip,setFlip] =useState(false)
+    const [height,setHeight] =useState('initial')
+
+    const frontEl=useRef()
+    const backEl=useRef()
+
+    function setMaxHeight() {
+        const frontHeight = frontEl.current.getBoundingClientRect().height
+        const backHeight = backEl.current.getBoundingClientRect().height
+        setHeight(Math.max(frontHeight,backHeight, 100))
+    }
+
+    useEffect(setMaxHeight,
+    [card.question, card.answer, card.options])
+
+    useEffect(() => {
+        window.addEventListener('resize',setMaxHeight)
+        return () => window.removeEventListener('resize', setMaxHeight)
+    },[])
+
 
 return (
     <div
         className={`card ${flip ? `flip` : ``}`}
+        style={{height: height}}
         onClick={() => setFlip(!flip)}>
 
-        <div className="front">
+        <div className="front" ref={frontEl}>
             {card.question}
             <div className="flashcard-options">
                 {card.options.map(option => {
@@ -16,7 +36,7 @@ return (
                 })}
             </div>
         </div>
-        <div className='back'>{card.answer}</div>
+        <div className='back' ref={backEl}>{card.answer}</div>
     </div>
 )
 }
